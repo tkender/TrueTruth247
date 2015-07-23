ebf7ca6775ebd3b42e58d28f72d689bb81d58868
unknown <tk3912@INCDTL01TK3912.ITServices.sbc.com>
2015-07-23 16:20:01 -0400

    Initial commit

---
+++ disclamer.js
@@ -0,0 +1,6 @@
+<!--
+function disclamer()
+{
+ 	alert('The external links provided for you on this website are connected directly to the answer of a specific question. Since many Christian ministries are represented by these links, other topics of discussion may be found on their sites which do not necessarily reflect the teachings of Plainfield Christian Church.')
+}
+//-->
